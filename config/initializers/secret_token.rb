0cc5be4502677672ad3e7c0bdafbeb1daece6f60
sastaputhra <sastaputhra@enova.com>
2013-05-01 16:35:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeBreaker::Application.config.secret_token = '1c69108bdb9e4c40644b25659b84c8927484c8fff2128ca32f55f3d72c39f55687e096e3d6adeefa14f9c2463fbb81852aa85a3a9063a89b2853be5fd762b3ef'
